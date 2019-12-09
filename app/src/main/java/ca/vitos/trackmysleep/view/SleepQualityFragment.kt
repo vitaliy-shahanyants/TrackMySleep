d4e20ad78e664c5e6b6d4c04eb311e16a994d58b
Vitaliy Shahanyants <vitaliys@clearcable.ca>
2019-12-09 11:17:08 -0500

    Setting up Sleep Quality

--- app/src/main/java/ca/vitos/trackmysleep/view/SleepQualityFragment.kt
+++ app/src/main/java/ca/vitos/trackmysleep/view/SleepQualityFragment.kt
@@ -6,8 +6,14 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import androidx.navigation.fragment.findNavController
 import ca.vitos.trackmysleep.R
+import ca.vitos.trackmysleep.database.SleepDatabase
 import ca.vitos.trackmysleep.databinding.FragmentSleepQualityBinding
+import ca.vitos.trackmysleep.viewmodel.sleepquality.SleepQualityViewModel
+import ca.vitos.trackmysleep.viewmodel.sleepquality.SleepQualityViewModelFactory
 
 class SleepQualityFragment: Fragment() {
 
@@ -19,6 +25,22 @@ class SleepQualityFragment: Fragment() {
         val binding: FragmentSleepQualityBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_sleep_quality,container,false)
 
         val application = requireNotNull(this.activity).application
+
+        val arguments = SleepQualityFragmentArgs.fromBundle(arguments!!)
+
+        val dataSource = SleepDatabase.getInstance(application).sleepDatabaseDao
+
+        val viewModelFactory = SleepQualityViewModelFactory(arguments.sleepNightKey,dataSource)
+        val viewModel = ViewModelProviders.of(this,viewModelFactory).get(SleepQualityViewModel::class.java)
+
+        binding.viewModel = viewModel
+
+        viewModel.navigateSleepTracker.observe(this, Observer {
+            if (it == true){
+                this.findNavController().navigate(SleepQualityFragmentDirections.actionSleepQualityFragmentToSleepQualityTracker())
+                viewModel.doneNavigating()
+            }
+        })
 
         return binding.root
     }
